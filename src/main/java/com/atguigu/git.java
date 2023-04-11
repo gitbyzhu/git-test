925a2189ef3c28a5b870d5d24ecaf57e9f4b3f1c
zhufeng <zhufeng@qq.com>
2023-04-11 14:27:46 +0800

    third commit

--- src/main/java/com/atguigu/git.java
+++ src/main/java/com/atguigu/git.java
@@ -10,5 +10,6 @@ public class git {
     public static void main(String[] args) {
         System.out.println("hello git1");
         System.out.println("hello git2");
+        System.out.println("hello git3");
     }
 }
